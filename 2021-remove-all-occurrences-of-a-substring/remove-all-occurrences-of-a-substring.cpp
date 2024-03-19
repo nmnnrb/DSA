71345e92403d555ccabd5932ad932a6d930b639f
nmnnrb <151563433+nmnnrb@users.noreply.github.com>
2024-03-20 02:27:44 +0530

    Time: 6 ms (23.20%) | Memory: 7.9 MB (72.73%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+     while(s.length() != 0 && s.find(part) < s.length()){
+       int st = s.find(part);
+       int e = part.length();
+        s.erase(st,e);
+        }
+        return s;
+
+    }
+    
+};
