942f972263f294c70a6c343d5b772d4a6fcaec69
nmnnrb <151563433+nmnnrb@users.noreply.github.com>
2024-09-23 15:40:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (5.13%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int solve(vector<int> nums ){
     int n = nums.size();
-          if(n==0)
-            return 0;
-        if(n== 1)
-            return nums[0];
+        //   if(n==0)
+        //     return 0;
+        // if(n== 1)
+        //     return nums[0];
         
         int prev1 = nums[0];
         int prev2 =0;
